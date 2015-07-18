58a544e111f6c7fbfd0a8bc60ae5decacac637ee
Andrew Jones <andyjones.ed@gmail.com>
2015-07-18 11:50:37 +0100

    Implements scraping off of Zoopla

--- scratch.py
+++ scratch.py
@@ -8,8 +8,14 @@ import re
 import json
 import urllib
 import urllib2
+import os
+import cPickle
+import itertools
+import time
 
+import scipy as sp
 import pandas as pd
+import zoopla
 
 ARRIVAL_TIME = 60*9
 DESTINATION = 'Euston'
@@ -46,13 +52,17 @@ def get_rents(stations):
     rents = {stations[d['id']]: d['rent'][str(N_BEDROOMS)] for d in json_data}
     rents = {k: WEEKS_PER_MONTH*float(v) for k, v in rents.iteritems() if float(v) > 0}
     return rents
+    
+def get_postcodes():
+    return pd.read_csv('tube-postcodes.txt', index_col=0, names=['name', 'postcode'])
 
-def get_dataframe(rents, times):
+def get_dataframe(rents, times, postcodes):
     df = pd.DataFrame(index=set(rents.keys()).intersection(times.keys()))
     df['rent'] = [rents[n] for n in df.index]
     df['time'] = [times[n] for n in df.index]
+    df['postcodes'] = postcodes
 
-    return df
+    return df.dropna()
 
 def get_border(df):
     by_time = df.sort('time')
@@ -72,8 +82,47 @@ def get_border(df):
         
     return border
     
-times = get_times()
-stations = get_stations()
-rents = get_rents(stations)
-df = get_dataframe(rents, times)
-border = get_border(df)
+#times = get_times()
+#stations = get_stations()
+#rents = get_rents(stations)
+#df = get_dataframe(rents, times)
+#border = get_border(df)
+
+#plt.scatter(df['time'], df['rent'])
+#plt.xlim([0, 60])
+#plt.xlabel('time')
+#plt.ylabel('rent per month')
+
+API_KEY = "7abvhabvegsnmtjxz9ybu9wj"
+
+def accumulate_rental_information(postcodes, file_name='data.pickle'):
+    if os.path.exists(file_name):
+        raise ValueError('File already exists!')
+    
+    cPickle.dump({}, open(file_name, 'w+'))   
+    
+    api = zoopla.api(version=1, api_key=API_KEY)
+    
+    for name, row in postcodes.iterrows():
+        print name, row['postcode']
+        listings = list(api.property_listings(area=row['postcode'], listing_status='rent', radius=0.5, minimum_beds=0, maximum_beds=1, order_by='age'))
+        current_store = cPickle.load(open(file_name, 'r'))
+        current_store[name] = listings
+        cPickle.dump(current_store, open(file_name, 'w+'))
+        
+        time.sleep(60)
+
+def get_rent_statistic(listings):
+    return WEEKS_PER_MONTH*sp.median([int(l.price) for l in listings])
+        
+def get_rent_statistics(file_name='data.pickle'):
+    store = cPickle.load(open(file_name, 'r'))
+    return {name: get_rent_statistic(listings) for name, listings in store.iteritems()}
+    
+#times = get_times()
+#stations = get_stations()
+#rents = get_rents(stations)
+#postcodes = get_postcodes()
+#df = get_dataframe(rents, times)
+#border = get_border(df)
+        
