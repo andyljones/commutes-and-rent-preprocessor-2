c63f64f7cd8fa61402cf1522f0806be21d2f3a18
Andrew Jones <andyjones.ed@gmail.com>
2015-08-02 00:06:54 +0100

    Adds support for unions and intersections of destination stations

--- commutes_and_rents.lua
+++ commutes_and_rents.lua
@@ -4,6 +4,10 @@ local cjson = require('cjson')
 
 require('gnuplot')
 
+local RENT_TOLERANCE = 1500
+local COMMUTE_TOLERANCE = 30
+local PERCENTILE = 75
+
 function load_commute_lengths()
   return torch.load('commute_lengths.t7')
 end
@@ -15,37 +19,97 @@ function load_rents()
   return cjson.decode(text)
 end
 
+function percentile(a, p)
+  local sorted = torch.sort(a)
+  local index = math.max((p/100.)*a:size(1) + 1, 1)
+  return sorted[index]
+end
+
+function short_name(name)
+  return std.string.split(name, ' Underground Station')[1]
+end
+
 local commute_lengths = load_commute_lengths()
 local rents = load_rents()
 
-local destination = 'Euston Underground Station'
-local relevant_data = {}
-for origin, commutes in pairs(commute_lengths) do
-  if #rents[origin] > 0 then
-    local commute = commutes[destination]
-    local rent = torch.median(torch.Tensor(rents[origin]))
-    relevant_data[origin] = {['commute']=commute, ['rent']=rent[1]}
+function get_data(destination)
+  local relevant_data = {}
+  for origin, commutes in pairs(commute_lengths) do
+    if #rents[origin] > 10 then
+      local commute = commutes[destination]
+      local rent = percentile(torch.Tensor(rents[origin]), PERCENTILE)
+      relevant_data[origin] = {['name']=origin, ['commute']=commute, ['rent']=rent, ['count']=#rents[origin]}
+    end
+  end
+  return relevant_data
+end
+
+function filter_data(data)
+  local filtered_data = {}
+  for k, v in pairs(data) do
+    if v.rent < RENT_TOLERANCE and v.commute < COMMUTE_TOLERANCE then
+      filtered_data[k] = v
+    end
+  end
+  return filtered_data
+end
+
+function get_filtered_data(destination)
+  return filter_data(get_data(destination))
+end
+
+function print_data(data)
+  local sorted = std.table.sort(std.table.keys(data), function (a,b) return data[a].commute < data[b].commute end)
+
+  for _, k in pairs(sorted) do
+    local v = data[k]
+    print(string.format('%2.0f mins, £%4.0f, %3d listings total, %s', v.commute, v.rent, v.count, short_name(v.name)))
   end
 end
 
--- function cmp(a, b)
---   return relevant_data[a].commute < relevant_data[b].commute
--- end
---
--- local commute_order = std.table.sort(std.table.keys(relevant_data), cmp)
-
--- for _, k in pairs(commute_order) do
---   local v = relevant_data[k]
---   print(string.format('%50s, %4.1f, %4d', k, v.commute, v.rent))
--- end
-
-local xs = {}
-local ys = {}
-for _, v in pairs(relevant_data) do
-  table.insert(xs, v.commute)
-  table.insert(ys, v.rent)
+function union(datasets)
+  local results = {}
+  for _, dataset in pairs(datasets) do
+    for _, elem in pairs(dataset) do
+      if results[elem.name] then
+        results[elem.name].commute = std.math.min(results[elem.name].commute, elem.commute)
+      else
+        results[elem.name] = elem
+      end
+    end
+  end
+  return results
 end
 
-gnuplot.setterm('aqua')
-gnuplot.plot(torch.Tensor(xs), torch.Tensor(ys), '+')
-gnuplot.figprint('out.png')
+function intersection(datasets)
+  local results = {}
+  for _, A in pairs(datasets) do
+    for _, elem in pairs(A) do
+      local is_in_all = true
+      for _, B in pairs(datasets) do
+        is_in_all = is_in_all and B[elem.name]
+      end
+
+      if is_in_all and results[elem.name] then
+        results[elem.name].commute = math.max(results[elem.name].commute, elem.commute)
+      elseif is_in_all then
+        results[elem.name] = elem
+      end
+    end
+  end
+  return results
+end
+
+function find_possibilities(A_targets, B_targets)
+  std.functional.map(function (target) return filter_data(get_data(target)) end)
+end
+
+local E = get_filtered_data('Euston Square Underground Station')
+local ES = get_filtered_data('Euston Underground Station')
+local KX = get_filtered_data('King\'s Cross St. Pancras Underground Station')
+
+local GP = get_filtered_data('Green Park Underground Station')
+
+local combination = intersection{GP, E}
+
+print_data(combination)
